5cdd0fc727ddaf23d91d9caad0a0c1dd282e8e1f
bCardenCode <brady.a.carden.-1@ou.edu>
2024-03-16 09:09:00 -0500

    Minor edits.

--- perceptron.py
+++ perceptron.py
@@ -2,14 +2,17 @@ from readApples import readApples
 import numpy as np
 
 class Perceptron():
-    def __init__(self, learningRate = 0.25):
+    def __init__(self, learningRate = 0.25, zeros = False):
         self.apples = readApples()
-        self.weights = [-0.5, 0.5, -0.5, 0.5, -0.5, 0.5, -0.5]
         self.fields = ["Size", "Weight", "Sweetness", "Crunchiness", "Juiciness", "Ripeness", "Acidity"]
         self.bias = 1
-        self.numInputs = 7
         self.learningRate = learningRate
         self.quality = "Quality"
+        
+        if zeros:
+            self.weights = [0, 0, 0, 0, 0, 0, 0]
+        else:
+            self.weights = [-0.5, 0.5, -0.5, 0.5, -0.5, 0.5, -0.5]    
     
     # Converts 'good' or 'bad' to 1 or -1
     def qualityToInt(self, quality):
@@ -21,7 +24,7 @@ class Perceptron():
     # Generates a apple quality prediction based on the inputs using current weights    
     def predict(self, apple):
         sum = self.bias
-        for i in range(self.numInputs):   
+        for i in range(len(self.fields)):   
             sum += float(apple[self.fields[i]]) * self.weights[i]
 
         if sum > 0:
@@ -36,7 +39,7 @@ class Perceptron():
         else:
             return weight + self.learningRate * (actual - prediction) * input
     
-    # Trains the perceptron on traininLength apples
+    # Trains the perceptron on trainingLength apples
     def train(self, trainingLength = 4000, resetWeights = True):
         
         # Resets weights
@@ -50,7 +53,7 @@ class Perceptron():
             actual = self.qualityToInt(apple[self.quality])
             
             # Updates weights
-            for i in range(self.numInputs):
+            for i in range(len(self.fields)):
                 input = float(apple[self.fields[i]])
                 self.weights[i] = self.updateWeight(prediction, actual, input, self.weights[i])    
     
@@ -63,7 +66,7 @@ class Perceptron():
             if appleQuality == self.predict(apple):
                 correctPredictions += 1
         
-        print("4000 Predictions completed")
-        print("Score: ", correctPredictions, "/", testLength, "(", correctPredictions / testLength * 100, "%)")        
+        print("4000 Predictions completed.")
+        print(f"Score: {correctPredictions} / {testLength} ({correctPredictions / testLength * 100}%)")        
             
                         
--- runPerceptron.py
+++ runPerceptron.py
@@ -1,19 +1,15 @@
 from perceptron import Perceptron
 
-perceptron = Perceptron()
-trainingLengths = [100, 500, 1000, 2000, 3000, 4000]
+perceptron = Perceptron(zeros=True)
 
 print("\nINITIAL TEST----")
 perceptron.test()
 print()
-"""
-for length in trainingLengths:
-    print("TRAINED on", length, "APPLES TEST----")
-    perceptron.train(length)
-    perceptron.test()
-    print()
-"""
 
-for _ in range(4):
-    perceptron.train(resetWeights=False)
-    perceptron.test()   
+print("TRAINED TEST----")
+perceptron.train()
+perceptron.test()
+print()
+
+
+  
